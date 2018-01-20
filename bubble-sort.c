46887fde30d5567c3951a21c5a3bb0ef726fcbee
Abdul Rehman Khan <abdulrehman3725@gmail.com>
2018-01-20 20:00:57 +0500

    Added the check for already sorted array

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 #define SIZE 5
-int list[SIZE] = {4,3,2,1,0};
+int list[SIZE] = {0,1,2,3,4};
 
 void sort()
 {
     int temp;
+
     for(int i = 0; i < SIZE - 1;  i++)
     {
+        bool swapped = true;
         for(int j = 0; j < SIZE - (i+1); j++)
         {
             if(list[j] > list[j+1])
@@ -14,7 +18,19 @@ void sort()
                 temp = list[j];
                 list[j] = list[j+1];
                 list[j+1] = temp;
+
+                swapped = false;
             }
+        }
+
+        if(swapped == true)
+        {
+            printf("Already Sorted \n");
+            break;
+        }
+        else
+        {
+            printf("NOt Already SOrted \n");
         }
     }
 }
